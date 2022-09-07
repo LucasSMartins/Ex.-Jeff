b3c4f69ecaaa3cb0c3ebe5bc15f9db81b96f7c2f
Lucas Martins <83207781+LucasSMartins@users.noreply.github.com>
2022-09-07 18:40:08 -0300

    Create index02.js

---
+++ ex - 09/index02.js
@@ -0,0 +1,18 @@
+const vetor1 = [1, 2, 4, 5, 8, 9]
+const vetor2 = [3, 6, 7, 11, 15]
+let vetorFinal = []
+const valorInformado = 2
+
+
+function True_OR_False(valor) {
+      if (vetor1.indexOf(valor) != -1) {return true
+      } else return false
+}
+
+
+
+
+
+
+
+console.log(True_OR_False(valorInformado))
